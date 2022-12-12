2b259c9e1d1c4518144b7741e0eef53d95bdda0c
Nfaúndez <nat.faundezc@gmail.com>
2022-12-12 14:04:17 -0300

    rutas spa

---
+++ src/componentes/home.js
@@ -0,0 +1,19 @@
+import { onNavigate } from '../main.js'; //se importa nuestra funcion para  darle el evento
+// home es la primera vista de la pantalla y nuestra primera carpeta de componentes
+export const Home = () => {
+
+  const HomeDiv = document.createElement('div');
+  const buttonRegister = document.createElement('button'); // botones de bienvenida
+  const buttonLogin = document.createElement('button');
+
+  buttonRegister.textContent = 'Registrate'; // nombre que contienen estos botones
+  buttonLogin.textContent = 'Inicia sesión';
+
+  buttonRegister.addEventListener('click', () => onNavigate('/register')); // eventos de los botones
+  buttonLogin.addEventListener('click', () => onNavigate('/login'));
+
+  HomeDiv.appendChild(buttonRegister); // appendchild inserta un nodo dentro de la estructura dom
+  HomeDiv.appendChild(buttonLogin);
+
+  return HomeDiv;
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,46 @@
 // Este es el punto de entrada de tu aplicacion
+import { Home } from './componentes/home.js'; // se importaron todas las funciones
+import { Register } from './componentes/register.js';
+import { Login } from './componentes/login.js';
 
-import { myFunction } from './lib/index.js';
+const rootDiv = document.getElementById('root');
+const routes = {
+  '/': Home, // aqui se asigna a las rutas
+  '/register': Register,
+  '/login': Login,
+};
 
-myFunction();
+rootDiv.appendChild (routes[window.location.pathname]());
+
+
+export const onNavigate = (pathname) => { //dar click e invocar esta funcion
+  window.history.pushState( 
+    {}, // es el estado
+    pathname, // titulo
+    window.location.origin + pathname, // ruta que queremos asignar
+    ); // con el + concatenamos el pathname
+    //while (rootDiv.firstChild) { // siempre que exista un primer hijo vamos a borrarlo
+     // rootDiv.removeChild(rootDiv.firstChild);
+     rootDiv.appendChild(routes[pathname]());
+     window.onpopstate = () => {
+      rootDiv.innerHTML = routes[window.location.pathname]()
+    }
+    
+      }
+    //};
+
+    const component = routes[window.location.pathname];
+    window.onpopstate = () => { // guarda una copia en nuestro historial
+      //while (rootDiv.firstChild) {
+        // rootDiv.removeChild(rootDiv.firstChild);
+        // rootDiv.appendChild(routes[window.location.pathname]());
+         }
+        //};
+
+        rootDiv.appendChild(component());
+
+
+
+
+
+
